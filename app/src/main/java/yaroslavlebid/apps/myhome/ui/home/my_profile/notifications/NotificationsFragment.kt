5139ffb7f397ab60c69cf26260d0400ee2b75ba5
Yaroslav Lebid <y.lebid@outlook.com>
2022-05-29 22:09:28 +0300

    created views for added apartments and notifications

---
+++ app/src/main/java/yaroslavlebid/apps/myhome/ui/home/my_profile/notifications/NotificationsFragment.kt
@@ -0,0 +1,7 @@
+package yaroslavlebid.apps.myhome.ui.home.my_profile.notifications
+
+import androidx.fragment.app.Fragment
+import yaroslavlebid.apps.myhome.R
+
+class NotificationsFragment : Fragment(R.layout.fragment_notifications) {
+}
